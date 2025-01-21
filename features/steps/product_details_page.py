51e17410b1df1ea8b768d0bde1908ebebbdab121
Afeez <karimonward@Afeezs-MacBook-Pro.local>
2025-01-21 17:25:00 +0000

    product_details_task5

--- features/steps/product_details_page.py
+++ features/steps/product_details_page.py
@@ -1,13 +1,11 @@
-from turtledemo.nim import COLOR
-
 from selenium.webdriver.common.by import By
 from behave import given, then
 from time import sleep
 
 
-COLOR_OPTIONS = (By.CSS_SELECTOR, 'a[rel="nofollow"]')
-SELECTED_COLOR = (By.CSS_SELECTOR, 'a[aria-live="polite"]')
 
+COLOR_OPTIONS = (By.CSS_SELECTOR, "div[aria-label='Carousel'] li img")
+SELECTED_COLOR = (By.CSS_SELECTOR, "[data-test='@web/VariationComponent'] div")
 
 
 @given('Open target product {product_id} page')
@@ -16,22 +14,20 @@ def open_target(context, product_id):
     sleep(5)
 
 
-
 @then('Verify user can click through colors')
 def click_and_verify_colors(context):
     expected_colors = ['Black', 'dark khaki', 'grey', 'navy/tan','white/sand/tan', 'white/navy/red']
     actual_colors = []
 
-    colors = context.driver.find_elements(*COLOR_OPTIONS)
+    colors = context.driver.find_elements(*COLOR_OPTIONS)  # [webelement1, webelement2, webelement3]
     for color in colors:
         color.click()
+
         selected_color = context.driver.find_element(*SELECTED_COLOR).text  # 'Color\nBlack'
         print('Current color', selected_color)
 
         selected_color = selected_color.split('\n')[1]  # remove 'Color\n' part, keep Black'
         actual_colors.append(selected_color)
         print(actual_colors)
-
-
 
 
---
+++ pages/search_results_page.py
@@ -0,0 +1,5 @@
+from pages.base_page import BasePage
+
+class SearchResultPage(BasePage):
+
+    def open_main
